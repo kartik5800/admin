0ee4c3068c13bbcbca1b949c982a904a46118a4c
krinali123 <paneliyakrinali2@gmail.com>
2022-05-18 08:48:05 +0530

    18-5 data gride

--- src/Container/Medicine.js
+++ src/Container/Medicine.js
@@ -1,4 +1,6 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
@@ -6,12 +8,70 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import { DataGrid } from '@mui/x-data-grid';
 
 
 
-
-export default function FormDialog() {
+function Medicine(props) {
     const [open, setOpen] = React.useState(false);
+    const [name, setname] = useState('');
+    const [price, setprice] = useState('');
+    const [quntity, setquntity] = useState('');
+    const [expiry, setexpiry] = useState('');
+    const [Data, setData] = useState([]);
+
+    const    columns = [
+        { field: 'id', headerName: 'ID', width: 90 },
+        {
+            field: 'Name',
+            headerName: 'name',
+            width: 150,
+            editable: false,
+        },
+        {
+            field: 'price',
+            headerName: 'price',
+            width: 150,
+            editable: false,
+        },
+        {
+            field: 'quntity',
+            headerName: 'quntity',
+            type: 'number',
+            width: 110,
+            editable: false,
+        },
+        {
+            field: 'expiry',
+            headerName: 'expiry',
+            sortable: false,
+            width: 160,
+        },
+    ];
+
+    const loadData = () => {
+        let localData = JSON.parse(localStorage.getItem("medicine"))
+        console.log("localData", localData);
+
+        if (loadData !== null) {
+            setData(localData);
+        }
+
+
+    }
+
+    useEffect(
+        () => {
+            loadData();
+        },
+        [])
+
+
+        const rows = [
+            { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+        ]
+
+
 
     const handleClickOpen = () => {
         setOpen(true);
@@ -21,53 +81,108 @@ export default function FormDialog() {
         setOpen(false);
     };
 
+
+    const handleSubmit = () => {
+        let localData = JSON.parse(localStorage.getItem("medicine"));
+
+        let Data = {
+            id: Math.floor(Math.random() * 1000 ),
+            name,
+            price,
+            quntity,
+            expiry
+        }
+
+        console.log(Data);
+
+        if
+            (localData === null) {
+            localStorage.setItem("medicine", JSON.stringify([Data]))
+        } else {
+            localData.push(Data);
+            localStorage.setItem("medicine", JSON.stringify(localData))
+        }
+        loadData()
+        setOpen(false);
+    }
+
+
     return (
         <div>
-            <h1 style={{ textAlign: "center" }}>medicine</h1>
-            <Button variant="outlined" onClick={handleClickOpen}>
-                Open form dialog
-            </Button>
-            <Dialog open={open} onClose={handleClose}>
-                <DialogTitle>MEDICINE</DialogTitle>
-                <DialogContent>
+            <Box>
+                <Container>
+                    <div>
+                        <Button variant="outlined" onClick={handleClickOpen}>
+                            ADD MEDICINE
+                        </Button>
+                        {
+                            Data !== null ?
+                        <div style={{ height: 400, width: '100%' }}>
+                            <DataGrid
+                                rows={rows}
+                                columns={columns}
+                                pageSize={5}
+                                rowsPerPageOptions={[5]}
+                                checkboxSelection
+                                disableSelectionOnClick
+                            />
+                        </div>
+                        : <p>empty table</p>
+}
+                        <Dialog open={open} onClose={handleClose}>
+                            <DialogTitle>Add medicine</DialogTitle>
+                            <DialogContent>
 
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        id="name"
-                        label="Enter medicine name"
-                        type="email"
-                        fullWidth
-                        variant="standard"
-                    />
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        id="quantity"
-                        label="Enter quantity"
-                        type="email"
-                        fullWidth
-                        variant="standard"
-                    />
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose}>Cancel</Button>
-                    <Button onClick={handleClose}>Submit</Button>
-                </DialogActions>
-            </Dialog>
+                                <TextField
+                                    autoFocus
+                                    margin="dense"
+                                    id="name"
+                                    label="medicine name"
+                                    fullWidth
+                                    variant="standard"
+                                    onChange={(e) => setname(e.target.value)}
+
+                                />
+                                <TextField
+                                    autoFocus
+                                    margin="dense"
+                                    id="price"
+                                    label="medicine price"
+                                    fullWidth
+                                    variant="standard"
+                                    onChange={(e) => setprice(e.target.value)}
+                                />
+                                <TextField
+                                    autoFocus
+                                    margin="dense"
+                                    id="quntity"
+                                    label="medicine quntity"
+                                    fullWidth
+                                    variant="standard"
+                                    onChange={(e) => setquntity(e.target.value)}
+                                />
+                                <TextField
+                                    autoFocus
+                                    margin="dense"
+                                    id="expiry"
+                                    label="medicine expiry"
+                                    fullWidth
+                                    variant="standard"
+                                    onChange={(e) => setexpiry(e.target.value)}
+                                />
+                            </DialogContent>
+                            <DialogActions>
+                                <Button onClick={handleClose}>Cancel</Button>
+                                <Button onClick={handleSubmit}>Submit</Button>
+                            </DialogActions>
+                        </Dialog>
+                    </div>
+                </Container>
+
+            </Box>
+
         </div>
     );
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
+export default Medicine;
